7f97eb4c305d187fb7c8d46f30a64780b061370a
ok_codebase <omid_k@posteo.de>
2024-07-13 07:50:58 +0000

    rebase

--- hello.py
+++ hello.py
@@ -2,7 +2,11 @@ def add(x, y):
     return x + y
 
 
+<<<<<<< HEAD
 
 
 result = add(4, 4)
 print(f"This is the sum: {2}, {4}, {result}")
+=======
+print(add(3, -8))
+>>>>>>> fc656c9 (change values (aws))
